df97ffa0d72ac95a960ddd7a2f87bb6aa088b0e8
AllTheCodeWeCannotSee <183735517@qq.com>
2022-10-06 15:47:33 +0800

    创建后端

---
+++ codeisthenight/src/main/java/com/codeisthenight/codeisthenight/CodeisthenightApplication.java
@@ -0,0 +1,13 @@
+package com.codeisthenight.codeisthenight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeisthenightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeisthenightApplication.class, args);
+    }
+
+}
---
+++ codeisthenight/src/test/java/com/codeisthenight/codeisthenight/CodeisthenightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeisthenight.codeisthenight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeisthenightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
