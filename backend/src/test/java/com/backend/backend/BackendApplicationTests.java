047b2e0b1386c3d7a0ed2138cc13b7a49e83c47a
AllTheCodeWeCannotSee <183735517@qq.com>
2022-10-06 15:49:34 +0800

    重命名后端

--- backend/src/test/java/com/backend/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/backend/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.codeisthenight.codeisthenight;
+package com.backend.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CodeisthenightApplicationTests {
+class BackendApplicationTests {
 
     @Test
     void contextLoads() {
