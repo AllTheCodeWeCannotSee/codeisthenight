ab528ba12aab99277a26b81fbf59a06391f8e6a5
AllTheCodeWeCannotSee <183735517@qq.com>
2022-10-09 15:40:04 +0800

    注册登录模块：spring-boot-starter-security、security与数据库对接

--- backend/src/main/java/com/codeisthenight/backend/controller/user/UserController.java
+++ backend/src/main/java/com/codeisthenight/backend/controller/user/UserController.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.codeisthenight.backend.mapper.UserMapper;
 import com.codeisthenight.backend.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +17,13 @@ public class UserController {
     @Autowired
     UserMapper userMapper;
 
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
     @GetMapping("/user/all/")
     public List<User> getAll() {
+
         return userMapper.selectList(null);
     }
 
@@ -32,7 +39,9 @@ public class UserController {
             @PathVariable int userId,
             @PathVariable String username,
             @PathVariable String password) {
-        User user = new User(userId, username, password);
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(password);
+        User user = new User(userId, username, encodedPassword);
         userMapper.insert(user);
         return "Add User Successfully";
     }
